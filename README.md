e99da099f6457082f4059158d155f4b692d9190a
OOP555 <natnaelassefa1997@gmail.com>
2025-05-04 10:52:22 +0300

    this is the first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # demo-
 demo 
+what is the deal man im hah ha h tha ,a
---
+++ name.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class name {
+   public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("what is the name of the assignment");
+        String subname = input.next();
+        System.out.printf("you will be doing %s assignment",subname);
+
+    }
+
+}
